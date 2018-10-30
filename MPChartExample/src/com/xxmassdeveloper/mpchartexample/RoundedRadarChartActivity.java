92344b97660be3b3a7777f76c326576de60a0187
rayworks <crayzhou@gmail.com>
2018-10-30 16:59:32 +0800

    Refine the chart usage in Main project

--- MPChartExample/src/com/xxmassdeveloper/mpchartexample/RoundedRadarChartActivity.java
+++ MPChartExample/src/com/xxmassdeveloper/mpchartexample/RoundedRadarChartActivity.java
@@ -4,16 +4,23 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PentagonRadarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.RadarData;
 import com.github.mikephil.charting.data.RadarDataSet;
 import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
 
 import java.util.ArrayList;
 
-public class RoundedRadarChartActivity extends RadarChartActivity {
+public class RoundedRadarChartActivity extends FragmentActivity {
 
     private int[] resDrawables = new int[]{
             R.drawable.ic_vocabulary_outlines,
@@ -22,31 +29,80 @@ public class RoundedRadarChartActivity extends RadarChartActivity {
             R.drawable.ic_expressions_outlines,
             R.drawable.ic_smile};
 
+    private PentagonRadarChart mChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(getRadarChartLayoutResource());
+
+        TextView tv = findViewById(R.id.textView);
+        tv.setTextColor(Color.BLACK);
+
+        mChart = findViewById(R.id.radar_chart);
+
+        initializeChart(new float[]{5, 4, 3, 3, 3});
+    }
+
+    private void initializeChart(float[] points) {
+        if (points == null || points.length != 5)
+            throw new IllegalArgumentException("The points param MUST contain 5 elements");
 
         int startColor = Color.argb(0xE6, 0, 0xBE, 0xFF);
         int endColor = Color.argb(0xE6, 0x14, 0x8C, 0xDC);
 
-        tv.setTextColor(Color.WHITE);
+        // map the point to the actual capacity value
+        float[] capacities = new float[points.length];
+        for (int i = 0; i < points.length; i++) {
+            float v = points[i];
+            if (Float.compare(3.0f, v) >= 0) {
+                capacities[i] = 30f;
+            } else if (Float.compare(4.0f, v) >= 0) {
+                capacities[i] = 60f;
+            } else {
+                capacities[i] = 90f;
+            }
+        }
+        setData(capacities);
+
+        mChart.animateXY(1400, 1400, Easing.EaseInOutQuad);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setTextSize(12f);
+        xAxis.setYOffset(0f);
+        xAxis.setXOffset(0f);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+            // Texts as the placeholders for displaying icons
+            private String[] mActivities = new String[]{"Burger", "Steak", "Salad", "Pasta", "Pizza"};
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return mActivities[(int) value % mActivities.length];
+            }
+        });
+
+        // reset the Label number for "3, 4, 5" points only
+        YAxis yAxis = mChart.getYAxis();
+        yAxis.setLabelCount(3, false);
+        yAxis.setTextSize(9f);
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(80f);
+        yAxis.setDrawLabels(false);
 
         Resources resources = getResources();
-
         // disable the further touch events
         Drawable[] drawables = new Drawable[resDrawables.length];
         for (int i = 0; i < resDrawables.length; i++) {
             drawables[i] = resources.getDrawable(resDrawables[i]);
         }
 
-        // reset the Label number for "3, 4, 5" points only
-        YAxis yAxis = mChart.getYAxis();
-        yAxis.setLabelCount(3, false);
-
         mChart.setBackgroundColor(Color.WHITE);
         mChart.setWebColor(Color.BLACK);
         mChart.setWebColorInner(Color.BLACK);
         mChart.setWebAlpha(0xFF);
+        mChart.setWebLineWidth(1f);
+        mChart.setWebLineWidthInner(1f);
 
         mChart.setTouchEnabled(false);
         mChart.setEdgeDrawables(drawables)
@@ -64,43 +120,39 @@ public class RoundedRadarChartActivity extends RadarChartActivity {
                 .setNumberVerticalOffset((int) resources.getDimension(R.dimen.offset_vertical));
     }
 
-    @Override
-    protected int getRadarChartLayoutResource() {
+    private int getRadarChartLayoutResource() {
         return R.layout.activity_roundedradarchart_noseekbar;
     }
 
-    @Override
-    public void setData() {
-        // test data
+    private void setData(float[] capacities) {
         // mapped data set from {100.f, 80.f, 60.f, 60.f, 60.f}
-        float[] capacities = new float[]{90, 60, 30, 30, 30};
+        // float[] capacities = new float[]{90, 60, 30, 30, 30};
         int cnt = capacities.length;
 
-        ArrayList<RadarEntry> entries2 = new ArrayList<>();
+        ArrayList<RadarEntry> entries = new ArrayList<>();
 
         // NOTE: The order of the entries when being added to the entries array determines their
         // position around the center of the chart.
         for (int i = 0; i < cnt; i++) {
-            entries2.add(new RadarEntry(capacities[i]));
+            entries.add(new RadarEntry(capacities[i]));
         }
 
-        RadarDataSet set2 = new RadarDataSet(entries2, "This Week");
+        RadarDataSet set = new RadarDataSet(entries, "");
 
         int color = Color.rgb(121, 162, 175);
         int fillColor = Color.rgb(121, 162, 175);
-        set2.setColor(color);
-        set2.setFillColor(fillColor);
-        set2.setDrawFilled(true);
-        set2.setFillAlpha(180);
-        set2.setLineWidth(2f);
-        set2.setDrawHighlightCircleEnabled(true);
-        set2.setDrawHighlightIndicators(false);
+        set.setColor(color);
+        set.setFillColor(fillColor);
+        set.setDrawFilled(true);
+        set.setFillAlpha(180);
+        set.setLineWidth(2f);
+        set.setDrawHighlightCircleEnabled(true);
+        set.setDrawHighlightIndicators(false);
 
         ArrayList<IRadarDataSet> sets = new ArrayList<IRadarDataSet>();
-        sets.add(set2);
+        sets.add(set);
 
         RadarData data = new RadarData(sets);
-        data.setValueTypeface(mTfLight);
         data.setValueTextSize(8f);
         data.setDrawValues(false);
         data.setValueTextColor(Color.BLACK);
@@ -108,8 +160,10 @@ public class RoundedRadarChartActivity extends RadarChartActivity {
         mChart.setData(data);
 
         // hide the label
-        mChart.setEdgeValueRadius(6);
         mChart.getLegend().setEnabled(false);
+
+        // hide the description
+        mChart.setDescription(null);
 
         mChart.invalidate();
     }
